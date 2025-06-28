d88f5eb75342799c90549de17cdd5f7cdadd55c1
Pradhumn <sirothiyapradhumn@gmail.com>
2025-06-29 01:13:30 +0530

    feat: initialize gridcert project with Vite, React, and Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <div className="">
+      <Button className="bg-amber-400">Click me</Button>
+    </div>
+  );
+}
+
+export default App;
