576591626f8410bb654947ee37ec27a17faeda05
Pradhumn <sirothiyapradhumn@gmail.com>
2025-06-29 17:20:27 +0530

    refactor: improve column definitions for clarity and consistency in CertGrid

--- src/utils/columnDef.jsx
+++ src/utils/columnDef.jsx
@@ -2,24 +2,35 @@ export const columnsCerts = [
   {
     header: "Name",
     id: "name",
-    accessorFn: row => {
+    accessorFn: (row) => {
       return row.name.split("/").slice(-4).join("/");
       // return row.name;
-    }
+    },
   },
   {
-    header: "Env",
-    accessorFn: row => row.tags.find(tag => tag.Key === "env")?.Value || "",
+    header: () => <div className="hidden md:block">Env</div>,
+    accessorFn: (row) => row.tags.find((tag) => tag.Key === "env")?.Value || "",
     id: "env",
+    cell: ({ row }) => (
+      <div className="hidden md:block">{row.getValue("env")}</div>
+    ),
   },
   {
-    header: "Product Name",
-    accessorFn: row => row.tags.find(tag => tag.Key === "product_name")?.Value || "",
+    header: () => <div className="hidden md:block">Product Name</div>,
+    accessorFn: (row) =>
+      row.tags.find((tag) => tag.Key === "product_name")?.Value || "",
     id: "product_name",
+    cell: ({ row }) => (
+      <div className="hidden md:block">{row.getValue("product_name")}</div>
+    ),
   },
   {
-    header: "Application Name",
-    accessorFn: row => row.tags.find(tag => tag.Key === "application_name")?.Value || "",
+    header: () => <div className="hidden md:block">Application Name</div>,
+    accessorFn: (row) =>
+      row.tags.find((tag) => tag.Key === "application_name")?.Value || "",
     id: "application_name",
+    cell: ({ row }) => (
+      <div className="hidden md:block">{row.getValue("application_name")}</div>
+    ),
   },
 ];
